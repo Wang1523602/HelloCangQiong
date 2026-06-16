750590a1a3c97e82c023bcf19c5a4598b7fef4c9
wangshuo <2889836723@qq.com>
2026-06-16 09:21:52 +0800

    页面查询模块更新完毕

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,13 +1,17 @@
 package com.sky.service;
 
+
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
     /**
      * 员工登录
+     *
      * @param employeeLoginDTO
      * @return
      */
@@ -16,9 +20,13 @@ public interface EmployeeService {
 
     /**
      * 新增员工
+     *
      * @param employeeDTO
      */
     void save(EmployeeDTO employeeDTO);
 
-
+    /*
+     * 分页查询
+     * */
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
